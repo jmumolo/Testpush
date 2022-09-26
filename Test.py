81d86b5899fe4a011fbd47f451eb116931b7e99a
jmumolo <requiemvii@aol.com>
2022-09-26 12:21:22 -0700

    added test.py

---
+++ Test.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+root = Tk()
+root.title("Sea Charts")
+root.configure(bg='#187EFF')
+
+Frame(height = 1000,width = 1280).pack()
+
+frame1 = Frame(root)
+frame1.pack(padx=10,pady=10)
+
+#label= Label(frame1,text="Map",justify=LEFT)
+#label.pack(padx=10,pady=500)
+#label.pack(side=RIGHT)
+
+
+root.mainloop()
